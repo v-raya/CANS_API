1e82063908a4f3b2ae73e34f5a4f7bd1a9e889e2
Denys Davydov <dom8833@ya.ru>
2018-06-12 16:22:39 -0700

    CANS-203. Social worker saves a CANS assessment. Added completedAs field to assessment.

---
+++ src/main/java/gov/ca/cwds/cans/domain/dto/assessment/AbstractAssessmentDto.java
@@ -0,0 +1,52 @@
+package gov.ca.cwds.cans.domain.dto.assessment;
+
+import static gov.ca.cwds.rest.api.domain.DomainObject.DATE_FORMAT;
+import static gov.ca.cwds.rest.api.domain.DomainObject.TIMESTAMP_ISO8601_FORMAT;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import gov.ca.cwds.cans.domain.dto.Dto;
+import gov.ca.cwds.cans.domain.dto.PersonDto;
+import gov.ca.cwds.cans.domain.dto.logging.CreationLoggable;
+import gov.ca.cwds.cans.domain.dto.logging.SubmitLoggable;
+import gov.ca.cwds.cans.domain.dto.logging.UpdateLoggable;
+import gov.ca.cwds.cans.domain.enumeration.AssessmentStatus;
+import gov.ca.cwds.cans.domain.enumeration.AssessmentType;
+import gov.ca.cwds.cans.domain.enumeration.CompletedAs;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/** @author denys.davydov */
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+public abstract class AbstractAssessmentDto extends Dto
+    implements CreationLoggable<AbstractAssessmentDto>,
+        UpdateLoggable<AbstractAssessmentDto>,
+        SubmitLoggable<AbstractAssessmentDto> {
+  private Long instrumentId;
+  private PersonDto person;
+  private AssessmentType assessmentType;
+  private AssessmentStatus status;
+  private CompletedAs completedAs;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT)
+  private LocalDate eventDate;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = TIMESTAMP_ISO8601_FORMAT)
+  private LocalDateTime createdTimestamp;
+
+  private PersonDto createdBy;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = TIMESTAMP_ISO8601_FORMAT)
+  private LocalDateTime updatedTimestamp;
+
+  private PersonDto updatedBy;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = TIMESTAMP_ISO8601_FORMAT)
+  private LocalDateTime submittedTimestamp;
+
+  private PersonDto submittedBy;
+}
--- src/main/java/gov/ca/cwds/cans/domain/dto/assessment/AssessmentDto.java
+++ src/main/java/gov/ca/cwds/cans/domain/dto/assessment/AssessmentDto.java
@@ -1,21 +1,8 @@
 package gov.ca.cwds.cans.domain.dto.assessment;
 
-import static gov.ca.cwds.rest.api.domain.DomainObject.DATE_FORMAT;
-import static gov.ca.cwds.rest.api.domain.DomainObject.TIMESTAMP_ISO8601_FORMAT;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import gov.ca.cwds.cans.domain.dto.Dto;
-import gov.ca.cwds.cans.domain.dto.PersonDto;
-import gov.ca.cwds.cans.domain.dto.logging.CreationLoggable;
-import gov.ca.cwds.cans.domain.dto.logging.SubmitLoggable;
-import gov.ca.cwds.cans.domain.dto.logging.UpdateLoggable;
-import gov.ca.cwds.cans.domain.enumeration.AssessmentStatus;
-import gov.ca.cwds.cans.domain.enumeration.AssessmentType;
 import gov.ca.cwds.cans.domain.json.AssessmentJson;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -25,29 +12,6 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 @EqualsAndHashCode(callSuper = true)
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-public class AssessmentDto extends Dto
-    implements CreationLoggable<AssessmentDto>,
-        UpdateLoggable<AssessmentDto>,
-        SubmitLoggable<AssessmentDto> {
-
-  private Long instrumentId;
-  private PersonDto person;
-  private AssessmentType assessmentType;
-  private AssessmentStatus status;
+public class AssessmentDto extends AbstractAssessmentDto {
   private AssessmentJson state;
-
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT)
-  private LocalDate eventDate;
-
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = TIMESTAMP_ISO8601_FORMAT)
-  private LocalDateTime createdTimestamp;
-  private PersonDto createdBy;
-
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = TIMESTAMP_ISO8601_FORMAT)
-  private LocalDateTime updatedTimestamp;
-  private PersonDto updatedBy;
-
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = TIMESTAMP_ISO8601_FORMAT)
-  private LocalDateTime submittedTimestamp;
-  private PersonDto submittedBy;
 }
