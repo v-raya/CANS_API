d983bd8dbd3ca387241b56e7ce3bdc6333d1b0f5
Volodymyr Petrusha <volodymyr.petrusha@osi.ca.gov>
2018-09-13 18:13:36 -0700

    person rules applied to assessment

--- src/main/java/gov/ca/cwds/cans/security/AssessmentReadAuthorizer.java
+++ src/main/java/gov/ca/cwds/cans/security/AssessmentReadAuthorizer.java
@@ -1,32 +1,5 @@
 package gov.ca.cwds.cans.security;
 
-import com.google.inject.Inject;
-import gov.ca.cwds.cans.dao.AssessmentDao;
-import gov.ca.cwds.cans.domain.entity.Assessment;
-import gov.ca.cwds.drools.DroolsConfiguration;
-
-public class AssessmentReadAuthorizer extends DroolsAuthorizer<Assessment, Long> {
-
-  private static final String CONFIGURATION_NAME = "authorization-rules";
-  private static final String AGENDA_GROUP_NAME = "assessment-read-authorization-rules";
-  @Inject
-  private AssessmentDao assessmentDao;
-
-  public AssessmentReadAuthorizer() {
-    super(new DroolsConfiguration<>(
-        CONFIGURATION_NAME,
-        AGENDA_GROUP_NAME,
-        CONFIGURATION_NAME));
-  }
-
-  @Override
-  protected boolean checkId(Long id) {
-    Assessment assessment = assessmentDao.find(id);
-    return checkInstance(assessment);
-  }
-
-  @Override
-  protected Long stringToId(String id) {
-    return Long.valueOf(id);
-  }
+public class AssessmentReadAuthorizer extends AssessmentWriteAuthorizer {
+  
 }
