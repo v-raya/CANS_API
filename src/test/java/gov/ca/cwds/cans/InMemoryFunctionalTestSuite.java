2c48b674cd26f4787e0c39dc15818074970f402b
Denys Davydov <denys.davydov@osi.ca.gov>
2018-10-16 10:32:51 -0700

    CANS-504. Supervisor Dashboard. Include all tests

--- src/test/java/gov/ca/cwds/cans/InMemoryFunctionalTestSuite.java
+++ src/test/java/gov/ca/cwds/cans/InMemoryFunctionalTestSuite.java
@@ -2,7 +2,18 @@ package gov.ca.cwds.cans;
 
 import static gov.ca.cwds.cans.test.util.ConfigurationProvider.CONFIG_FILE_PATH;
 
+import gov.ca.cwds.cans.rest.resource.AssessmentResourceAuthorizationTest;
+import gov.ca.cwds.cans.rest.resource.AssessmentResourceTest;
+import gov.ca.cwds.cans.rest.resource.AuthorizationResourceTest;
+import gov.ca.cwds.cans.rest.resource.CountyResourceTest;
+import gov.ca.cwds.cans.rest.resource.I18nResourceTest;
+import gov.ca.cwds.cans.rest.resource.InstrumentResourceTest;
+import gov.ca.cwds.cans.rest.resource.PersonResourceAuthorizationTest;
+import gov.ca.cwds.cans.rest.resource.PersonResourceTest;
+import gov.ca.cwds.cans.rest.resource.SecurityResourceTest;
+import gov.ca.cwds.cans.rest.resource.SensitivityTypeResourceTest;
 import gov.ca.cwds.cans.rest.resource.StaffResourceTest;
+import gov.ca.cwds.cans.rest.resource.SystemInformationResourceTest;
 import gov.ca.cwds.cans.test.InMemoryFunctionalRestClientTestRule;
 import gov.ca.cwds.cans.test.util.DatabaseHelper;
 import gov.ca.cwds.cans.test.util.FunctionalTestContextHolder;
@@ -27,18 +38,18 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses(
     value = {
-      //      AuthorizationResourceTest.class,
-      //      SystemInformationResourceTest.class,
-      //      AssessmentResourceTest.class,
-      //      CountyResourceTest.class,
-      //      I18nResourceTest.class,
-      //      InstrumentResourceTest.class,
-      //      PersonResourceTest.class,
-      //      SecurityResourceTest.class,
-      //      SensitivityTypeResourceTest.class,
+      AuthorizationResourceTest.class,
+      SystemInformationResourceTest.class,
+      AssessmentResourceTest.class,
+      CountyResourceTest.class,
+      I18nResourceTest.class,
+      InstrumentResourceTest.class,
+      PersonResourceTest.class,
+      SecurityResourceTest.class,
+      SensitivityTypeResourceTest.class,
       StaffResourceTest.class,
-      //      PersonResourceAuthorizationTest.class,
-      //      AssessmentResourceAuthorizationTest.class,
+      PersonResourceAuthorizationTest.class,
+      AssessmentResourceAuthorizationTest.class,
     })
 public class InMemoryFunctionalTestSuite {
 
