42b2245292e3224f9794f9b26ab43a8feff69762
Denys Davydov <denys.davydov@osi.ca.gov>
2018-10-22 11:39:34 -0700

    CANS-607. Supervisor Dashboard shows Total Client Count

--- src/main/java/gov/ca/cwds/cans/service/StaffService.java
+++ src/main/java/gov/ca/cwds/cans/service/StaffService.java
@@ -7,6 +7,7 @@ import gov.ca.cwds.cans.domain.dto.facade.StaffStatisticsDto;
 import gov.ca.cwds.cans.domain.entity.facade.Statistics;
 import gov.ca.cwds.cans.domain.mapper.StaffStatisticMapper;
 import gov.ca.cwds.data.legacy.cms.dao.StaffPersonDao;
+import gov.ca.cwds.data.legacy.cms.entity.facade.ClientCountByStaff;
 import gov.ca.cwds.data.legacy.cms.entity.facade.StaffBySupervisor;
 import gov.ca.cwds.security.utils.PrincipalUtils;
 import io.dropwizard.hibernate.UnitOfWork;
@@ -24,26 +25,56 @@ public class StaffService {
   @Inject private StaffStatisticMapper staffStatisticMapper;
 
   public Collection<StaffStatisticsDto> getStaffStatisticsBySupervisor() {
-    final Collection<StaffBySupervisor> staffList =
-        getStaffBySupervisor(PrincipalUtils.getPrincipal().getStaffId());
-    final Set<String> staffRacfIds =
-        staffList.stream().map(StaffBySupervisor::getRacfId).collect(Collectors.toSet());
-    if (staffRacfIds.isEmpty()) {
+    final String currentStaffId = PrincipalUtils.getPrincipal().getStaffId();
+    final Collection<StaffBySupervisor> staffList = getStaffBySupervisor(currentStaffId);
+    if (staffList.isEmpty()) {
       return Collections.emptyList();
     }
+
+    final Map<String, ClientCountByStaff> clientCountByStaffMap =
+        fetchClientCountByStaffMap(staffList);
+
+    final Set<String> staffRacfIds =
+        staffList.stream().map(StaffBySupervisor::getRacfId).collect(Collectors.toSet());
     final Map<String, Statistics> statisticsMap =
         statisticsService.getStaffStatistics(staffRacfIds);
+
     final Collection<StaffStatisticsDto> results = new ArrayList<>(staffRacfIds.size());
     for (StaffBySupervisor staff : staffList) {
       final Statistics statistics = statisticsMap.get(staff.getRacfId());
-      final StaffStatisticsDto statisticsDto = staffStatisticMapper.toDto(staff, statistics);
+      final ClientCountByStaff clientCount = clientCountByStaffMap.get(staff.getIdentifier());
+      final StaffStatisticsDto statisticsDto =
+          staffStatisticMapper.toDto(staff, statistics, clientCount);
       results.add(statisticsDto);
     }
     return results;
   }
 
+  private Map<String, ClientCountByStaff> fetchClientCountByStaffMap(
+      Collection<StaffBySupervisor> staffList) {
+    final Set<String> staffIds =
+        staffList.stream().map(StaffBySupervisor::getIdentifier).collect(Collectors.toSet());
+    final Collection<ClientCountByStaff> clientCounts = getClientCountByStaffIds(staffIds);
+    final Map<String, ClientCountByStaff> resultMap =
+        clientCounts
+            .stream()
+            .collect(Collectors.toMap(ClientCountByStaff::getIdentifier, dto -> dto));
+    // add default empty values
+    staffIds
+        .stream()
+        .filter(id -> !resultMap.containsKey(id))
+        .forEach(id -> resultMap.put(id, new ClientCountByStaff(id, 0, 0)));
+    return resultMap;
+  }
+
   @UnitOfWork(CMS)
   public Collection<StaffBySupervisor> getStaffBySupervisor(final String supervisorId) {
     return staffPersonDao.findStaffBySupervisorId(supervisorId);
+  }
+
+  @UnitOfWork(CMS)
+  public Collection<ClientCountByStaff> getClientCountByStaffIds(
+      final Collection<String> staffIds) {
+    return staffPersonDao.countClientsByStaffIds(staffIds);
   }
 }
