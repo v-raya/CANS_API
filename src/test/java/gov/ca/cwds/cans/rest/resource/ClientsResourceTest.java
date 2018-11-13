265e03049709fc99e37014c652570ec72a28267c
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-11-13 14:35:55 -0800

    client resource test

--- src/test/java/gov/ca/cwds/cans/rest/resource/ClientsResourceTest.java
+++ src/test/java/gov/ca/cwds/cans/rest/resource/ClientsResourceTest.java
@@ -2,6 +2,7 @@ package gov.ca.cwds.cans.rest.resource;
 
 import gov.ca.cwds.cans.Constants.API;
 import gov.ca.cwds.cans.domain.dto.person.ClientDto;
+import gov.ca.cwds.cans.test.util.FixtureReader;
 import java.io.IOException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -10,31 +11,36 @@ import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
-/** @author CWDS TPT-2 Team */
+/**
+ * @author CWDS TPT-2 Team
+ */
 public class ClientsResourceTest extends AbstractFunctionalTest {
 
-  private static final String CLIENT_CMS_ID = "AbA4BJy0Aq";
-  private static final String CLIENT_CMS_BASE10_KEY = "0602-0480-3081-8000672";
+  private static final String AUTHORIZED_USER = "fixtures/perry-account/0ki-napa-all.json";
+  private static final String CLIENT = "fixtures/client-of-0Ki-rw-assignment.json";
+
 
   @Test
   public void doGetClient_success() throws IOException {
+    ClientDto expected = FixtureReader
+        .readObject(CLIENT, ClientDto.class);
     Response response =
         clientTestRule
-            .withSecurityToken(AUTHORIZED_ACCOUNT_FIXTURE)
-            .target(API.CLIENTS + "/" + CLIENT_CMS_ID)
+            .withSecurityToken(AUTHORIZED_USER)
+            .target(API.CLIENTS + "/" + expected.getIdentifier())
             .request(MediaType.APPLICATION_JSON_TYPE)
             .get();
     Assert.assertThat(response.getStatus(), Matchers.equalTo(HttpStatus.SC_OK));
 
     ClientDto clientDto = response.readEntity(ClientDto.class);
-    Assert.assertEquals(CLIENT_CMS_BASE10_KEY, clientDto.getExternalId());
+    Assert.assertEquals(expected.getExternalId(), clientDto.getExternalId());
   }
 
   @Test
   public void doGetClient_notFound() throws IOException {
     Response response =
         clientTestRule
-            .withSecurityToken(AUTHORIZED_ACCOUNT_FIXTURE)
+            .withSecurityToken(AUTHORIZED_USER)
             .target(API.CLIENTS + "/" + "-1")
             .request(MediaType.APPLICATION_JSON_TYPE)
             .get();
