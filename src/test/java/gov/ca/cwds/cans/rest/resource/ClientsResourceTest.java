5266d84ca284fa55f4b277d617d3c6dfb3408219
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-11-13 15:56:16 -0800

    person resource unauthorized test

--- src/test/java/gov/ca/cwds/cans/rest/resource/ClientsResourceTest.java
+++ src/test/java/gov/ca/cwds/cans/rest/resource/ClientsResourceTest.java
@@ -16,9 +16,10 @@ import org.junit.Test;
  */
 public class ClientsResourceTest extends AbstractFunctionalTest {
 
-  private static final String AUTHORIZED_USER = "fixtures/perry-account/0ki-napa-all.json";
+  private static final String AUTHORIZED_USER_NAPA = "fixtures/perry-account/0ki-napa-all.json";
+  private static final String UNAUTHORIZED_USER = AUTHORIZED_EL_DORADO_ACCOUNT_FIXTURE;
   private static final String CLIENT = "fixtures/client-of-0Ki-rw-assignment.json";
-
+  private static final String SEALED_CLIENT_MARLIN = "fixtures/sealed-client-marlin.json";
 
   @Test
   public void doGetClient_success() throws IOException {
@@ -26,7 +27,7 @@ public class ClientsResourceTest extends AbstractFunctionalTest {
         .readObject(CLIENT, ClientDto.class);
     Response response =
         clientTestRule
-            .withSecurityToken(AUTHORIZED_USER)
+            .withSecurityToken(AUTHORIZED_USER_NAPA)
             .target(API.CLIENTS + "/" + expected.getIdentifier())
             .request(MediaType.APPLICATION_JSON_TYPE)
             .get();
@@ -37,10 +38,23 @@ public class ClientsResourceTest extends AbstractFunctionalTest {
   }
 
   @Test
+  public void doGetClient_unauthorized() throws IOException {
+    ClientDto expected = FixtureReader
+        .readObject(SEALED_CLIENT_MARLIN, ClientDto.class);
+    Response response =
+        clientTestRule
+            .withSecurityToken(UNAUTHORIZED_USER)
+            .target(API.CLIENTS + "/" + expected.getIdentifier())
+            .request(MediaType.APPLICATION_JSON_TYPE)
+            .get();
+    Assert.assertThat(response.getStatus(), Matchers.equalTo(HttpStatus.SC_FORBIDDEN));
+  }
+
+  @Test
   public void doGetClient_notFound() throws IOException {
     Response response =
         clientTestRule
-            .withSecurityToken(AUTHORIZED_USER)
+            .withSecurityToken(AUTHORIZED_USER_NAPA)
             .target(API.CLIENTS + "/" + "-1")
             .request(MediaType.APPLICATION_JSON_TYPE)
             .get();
