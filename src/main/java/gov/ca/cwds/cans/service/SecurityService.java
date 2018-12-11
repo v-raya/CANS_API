5aa7b2e4002911f62611fb825cdddfaa7d059571
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2018-12-10 16:44:08 -0800

    security service permission check bug fixed

--- src/main/java/gov/ca/cwds/cans/service/SecurityService.java
+++ src/main/java/gov/ca/cwds/cans/service/SecurityService.java
@@ -1,14 +1,21 @@
 package gov.ca.cwds.cans.service;
 
+import gov.ca.cwds.security.utils.PrincipalUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.AuthorizationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SecurityService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(SecurityService.class);
 
   public boolean isPermitted(String permission) {
     try {
       return SecurityUtils.getSubject().isPermitted(permission);
     } catch (AuthorizationException e) { // NOSONAR
+      LOG.info("user: " + PrincipalUtils.getPrincipal().getStaffId() + " doesn't have permission: "
+          + permission, e.getCause());
       return false;
     }
   }
