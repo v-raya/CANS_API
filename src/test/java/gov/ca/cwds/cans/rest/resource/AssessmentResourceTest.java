99f72245225d482f5c2e4fdc8531aa8d4303068e
Leonid Marushevskyi <leonid.marushevskiy@osi.ca.gov>
2018-12-18 13:01:30 -0800

    Assessment cleanup in tests

--- src/test/java/gov/ca/cwds/cans/rest/resource/AssessmentResourceTest.java
+++ src/test/java/gov/ca/cwds/cans/rest/resource/AssessmentResourceTest.java
@@ -1,6 +1,10 @@
 package gov.ca.cwds.cans.rest.resource;
 
 import static gov.ca.cwds.cans.Constants.API.ASSESSMENTS;
+import static gov.ca.cwds.cans.Constants.API.CHANGELOG;
+import static gov.ca.cwds.cans.Constants.API.SEARCH;
+import static gov.ca.cwds.cans.domain.enumeration.AssessmentStatus.COMPLETED;
+import static gov.ca.cwds.cans.domain.enumeration.AssessmentStatus.IN_PROGRESS;
 import static gov.ca.cwds.cans.test.util.FixtureReader.readObject;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
@@ -8,21 +12,27 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 
+import gov.ca.cwds.cans.domain.dto.CountyDto;
 import gov.ca.cwds.cans.domain.dto.assessment.AssessmentDto;
+import gov.ca.cwds.cans.domain.dto.assessment.AssessmentMetaDto;
+import gov.ca.cwds.cans.domain.dto.assessment.SearchAssessmentRequest;
+import gov.ca.cwds.cans.domain.dto.changelog.AssessmentChangeLogDto;
 import gov.ca.cwds.cans.domain.dto.person.ClientDto;
+import gov.ca.cwds.cans.domain.enumeration.AssessmentChangeType;
 import gov.ca.cwds.cans.domain.enumeration.AssessmentStatus;
+import gov.ca.cwds.cans.domain.enumeration.ServiceSource;
 import gov.ca.cwds.rest.exception.BaseExceptionResponse;
 import gov.ca.cwds.rest.exception.IssueDetails;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Stack;
 import java.util.stream.Collectors;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.apache.http.HttpStatus;
-import org.junit.After;
 import org.junit.Test;
 
 /** @author denys.davydov */
@@ -40,19 +50,6 @@ public class AssessmentResourceTest extends AbstractFunctionalTest {
   private static final String AUTHORIZED_USER = "fixtures/perry-account/0ki-napa-all.json";
   private static final String CASE_OR_REFERRAL_CMS_ID = "C6vN5DG0Aq";
   private static final String CASE_OR_REFERRAL_CMS_BASE10_KEY = "0687-9473-7673-8000672";
-  private final Stack<AssessmentDto> cleanUpAssessments = new Stack<>();
-
-  @After
-  public void tearDown() throws IOException {
-    while (!cleanUpAssessments.empty()) {
-      AssessmentDto assessmentToDelete = cleanUpAssessments.pop();
-      clientTestRule
-          .withSecurityToken(AUTHORIZED_USER)
-          .target(ASSESSMENTS + SLASH + assessmentToDelete.getId())
-          .request(MediaType.APPLICATION_JSON_TYPE)
-          .delete();
-    }
-  }
 
   @Test
   public void postAssessment_ignoresInputLogInfo() throws IOException {
@@ -70,6 +67,7 @@ public class AssessmentResourceTest extends AbstractFunctionalTest {
             .request(MediaType.APPLICATION_JSON_TYPE)
             .post(Entity.entity(inputAssessment, MediaType.APPLICATION_JSON_TYPE))
             .readEntity(AssessmentDto.class);
+    pushToCleanUpStack(actualAssessment.getId(), AUTHORIZED_NAPA_ACCOUNT_FIXTURE);
 
     // then
     assertThat(
@@ -80,9 +78,6 @@ public class AssessmentResourceTest extends AbstractFunctionalTest {
     assertThat(actualAssessment.getUpdatedTimestamp(), is(nullValue()));
     assertThat(actualAssessment.getCompletedBy(), is(nullValue()));
     assertThat(actualAssessment.getCompletedTimestamp(), is(nullValue()));
-
-    // clean up
-    cleanUpAssessments.push(actualAssessment);
   }
 
   @Test
@@ -98,6 +93,7 @@ public class AssessmentResourceTest extends AbstractFunctionalTest {
             .target(ASSESSMENTS)
             .request(MediaType.APPLICATION_JSON_TYPE)
             .post(Entity.entity(inputAssessment, MediaType.APPLICATION_JSON_TYPE));
+    pushToCleanUpStack(postResponse.readEntity(AssessmentDto.class).getId(), AUTHORIZED_ACCOUNT_FIXTURE);
 
     // then
     assertThat(postResponse.getStatus(), is(HttpStatus.SC_UNPROCESSABLE_ENTITY));
@@ -129,7 +125,210 @@ public class AssessmentResourceTest extends AbstractFunctionalTest {
   }
 
   @Test
-  public void putAssessment1_validationError_whenUpdatingConductedByOnCompleted()
+  public void postAssessment_failed_whenSubmittingInProgressWithNoAge() throws IOException {
+    // given
+    final ClientDto person = readObject(FIXTURE_POST_PERSON, ClientDto.class);
+    final AssessmentDto inputAssessment =
+        readObject(FIXTURE_POST_NO_AGE_INVALID, AssessmentDto.class);
+    inputAssessment.setPerson(person);
+
+    // when
+    final Response postResponse =
+        clientTestRule
+            .withSecurityToken(AUTHORIZED_ACCOUNT_FIXTURE)
+            .target(ASSESSMENTS)
+            .request(MediaType.APPLICATION_JSON_TYPE)
+            .post(Entity.entity(inputAssessment, MediaType.APPLICATION_JSON_TYPE));
+    pushToCleanUpStack(postResponse.readEntity(AssessmentDto.class).getId(), AUTHORIZED_ACCOUNT_FIXTURE);
+
+    // then
+    assertThat(postResponse.getStatus(), is(HttpStatus.SC_UNPROCESSABLE_ENTITY));
+    final BaseExceptionResponse exceptionResponse =
+        postResponse.readEntity(BaseExceptionResponse.class);
+    final List<String> itemCodes =
+        exceptionResponse
+            .getIssueDetails()
+            .stream()
+            .map(IssueDetails::getProperty)
+            .collect(Collectors.toList());
+    assertThat(itemCodes.size(), is(1));
+    assertThat(itemCodes, containsInAnyOrder("state.under_six"));
+  }
+
+  @Test
+  public void searchAssessments_findsFourSortedRecords() throws IOException {
+    // given
+    final List<Long> assessmentIds = new ArrayList<>();
+    final ClientDto person = readObject(FIXTURE_PERSON, ClientDto.class);
+    final ClientDto otherPerson = readObject(FIXTURE_PERSON, ClientDto.class);
+    otherPerson.setIdentifier("aaaaaaaaaa");
+    final AssessmentDto assessment = readObject(FIXTURE_POST, AssessmentDto.class);
+    final List<Object[]> properties =
+        Arrays.asList(
+            new Object[] {
+                person, IN_PROGRESS, LocalDate.of(2010, 1, 1), AUTHORIZED_NAPA_ACCOUNT_FIXTURE
+            },
+            new Object[] {
+                person, IN_PROGRESS, LocalDate.of(2015, 10, 10), AUTHORIZED_NAPA_ACCOUNT_FIXTURE
+            },
+            // out of search results because of the other person
+            new Object[] {
+                otherPerson, IN_PROGRESS, LocalDate.of(2015, 10, 10), AUTHORIZED_NAPA_ACCOUNT_FIXTURE
+            },
+            new Object[] {
+                person, COMPLETED, LocalDate.of(2010, 1, 1), AUTHORIZED_NAPA_ACCOUNT_FIXTURE
+            },
+            new Object[] {
+                person, COMPLETED, LocalDate.of(2015, 10, 10), AUTHORIZED_NAPA_ACCOUNT_FIXTURE
+            }
+            /*, Authorization going to be reworked
+            // out of search results because of the other created by user
+            new Object[] {
+              person, COMPLETED, LocalDate.of(2015, 10, 10), NOT_AUTHORIZED_ACCOUNT_FIXTURE
+            }*/
+        );
+
+    for (Object[] property : properties) {
+      final AssessmentDto newAssessment =
+          postAssessment(
+              assessment,
+              (ClientDto) property[0],
+              (AssessmentStatus) property[1],
+              (LocalDate) property[2],
+              (String) property[3]);
+      assessmentIds.add(newAssessment.getId());
+      if (newAssessment.getId() != null) {
+        pushToCleanUpStack(newAssessment.getId(), AUTHORIZED_NAPA_ACCOUNT_FIXTURE);
+      }
+    }
+    // when
+    final Entity<SearchAssessmentRequest> searchRequest =
+        Entity.entity(
+            new SearchAssessmentRequest().setClientIdentifier(person.getIdentifier()),
+            MediaType.APPLICATION_JSON_TYPE);
+    final AssessmentMetaDto[] actualResults =
+        clientTestRule
+            .withSecurityToken(AUTHORIZED_NAPA_ACCOUNT_FIXTURE)
+            .target(ASSESSMENTS + SLASH + SEARCH)
+            .request(MediaType.APPLICATION_JSON_TYPE)
+            .post(searchRequest)
+            .readEntity(AssessmentMetaDto[].class);
+
+    // then
+    assertThat(actualResults.length, is(4));
+    assertThat(actualResults[0].getId(), is(assessmentIds.get(1)));
+    assertThat(actualResults[1].getId(), is(assessmentIds.get(0)));
+    assertThat(actualResults[2].getId(), is(assessmentIds.get(4)));
+    assertThat(actualResults[3].getId(), is(assessmentIds.get(3)));
+  }
+
+  @Test
+  public void getChangeLog_findsFourSortedRecords() throws IOException {
+    // given
+    final ClientDto person = readObject(FIXTURE_PERSON, ClientDto.class);
+    final AssessmentDto assessment = readObject(FIXTURE_POST, AssessmentDto.class);
+    final List<Object[]> properties =
+        Arrays.asList(
+            new Object[] {
+                person, IN_PROGRESS, LocalDate.of(2018, 1, 1), AUTHORIZED_NAPA_ACCOUNT_FIXTURE
+            },
+            new Object[] {
+                person, IN_PROGRESS, LocalDate.of(2018, 2, 1), AUTHORIZED_NAPA_ACCOUNT_FIXTURE
+            },
+            new Object[] {
+                person, IN_PROGRESS, LocalDate.of(2018, 5, 1), AUTHORIZED_NAPA_ACCOUNT_FIXTURE
+            },
+            new Object[] {
+                person, COMPLETED, LocalDate.of(2018, 10, 1), AUTHORIZED_NAPA_ACCOUNT_FIXTURE
+            });
+    Long id = null;
+    for (Object[] property : properties) {
+      final AssessmentDto newAssessment;
+      if (id == null) {
+        newAssessment =
+            postAssessment(
+                assessment,
+                (ClientDto) property[0],
+                (AssessmentStatus) property[1],
+                (LocalDate) property[2],
+                (String) property[3]);
+
+        id = newAssessment.getId();
+        pushToCleanUpStack(newAssessment.getId(), AUTHORIZED_NAPA_ACCOUNT_FIXTURE);
+
+      } else {
+        newAssessment =
+            putAssessment(
+                assessment,
+                (ClientDto) property[0],
+                (AssessmentStatus) property[1],
+                (LocalDate) property[2],
+                (String) property[3],
+                id);
+      }
+      if (newAssessment.getId() != null) {}
+    }
+    // when
+    final AssessmentChangeLogDto[] actualResults =
+        clientTestRule
+            .withSecurityToken(AUTHORIZED_NAPA_ACCOUNT_FIXTURE)
+            .target(ASSESSMENTS + SLASH + String.valueOf(id) + SLASH + CHANGELOG)
+            .request(MediaType.APPLICATION_JSON_TYPE)
+            .get()
+            .readEntity(AssessmentChangeLogDto[].class);
+
+    // then
+    assertThat(actualResults.length, is(4));
+    assertThat(actualResults[0].getEntityId(), is(id));
+    assertThat(actualResults[1].getEntityId(), is(id));
+    assertThat(actualResults[2].getEntityId(), is(id));
+    assertThat(actualResults[3].getEntityId(), is(id));
+
+    assertThat(actualResults[0].getAssessmentChangeType(), is(AssessmentChangeType.COMPLETED));
+    assertThat(actualResults[1].getAssessmentChangeType(), is(AssessmentChangeType.SAVED));
+    assertThat(actualResults[2].getAssessmentChangeType(), is(AssessmentChangeType.SAVED));
+    assertThat(actualResults[3].getAssessmentChangeType(), is(AssessmentChangeType.CREATED));
+  }
+
+  @Test
+  public void putAssessment_notUpdatingCountyAndCaseId_whenUpdatingAssessment() throws IOException {
+    // given
+    final ClientDto person = readObject(FIXTURE_PERSON, ClientDto.class);
+    final AssessmentDto assessment = readObject(FIXTURE_POST, AssessmentDto.class);
+    assessment.setPerson(person);
+    assessment.setServiceSource(ServiceSource.CASE);
+    assessment.setServiceSourceId(CASE_OR_REFERRAL_CMS_ID);
+    final AssessmentDto postedAssessment =
+        clientTestRule
+            .withSecurityToken(AUTHORIZED_NAPA_ACCOUNT_FIXTURE)
+            .target(ASSESSMENTS)
+            .request(MediaType.APPLICATION_JSON_TYPE)
+            .post(Entity.entity(assessment, MediaType.APPLICATION_JSON_TYPE))
+            .readEntity(AssessmentDto.class);
+    pushToCleanUpStack(postedAssessment.getId(), AUTHORIZED_NAPA_ACCOUNT_FIXTURE);
+
+    // when
+    postedAssessment.setCounty((CountyDto) new CountyDto().setName("Sacramento").setId(1L));
+    assessment.setServiceSourceId("otherId000");
+    postedAssessment.setConductedBy("John Smith");
+    final AssessmentDto actualAssessment =
+        clientTestRule
+            .withSecurityToken(AUTHORIZED_NAPA_ACCOUNT_FIXTURE)
+            .target(ASSESSMENTS + SLASH + postedAssessment.getId())
+            .request(MediaType.APPLICATION_JSON_TYPE)
+            .put(Entity.entity(postedAssessment, MediaType.APPLICATION_JSON_TYPE))
+            .readEntity(AssessmentDto.class);
+
+    // then
+    assertThat(actualAssessment.getCounty().getId(), is(28L));
+    assertThat(actualAssessment.getServiceSource(), is(ServiceSource.CASE));
+    assertThat(actualAssessment.getServiceSourceId(), is(CASE_OR_REFERRAL_CMS_ID));
+    assertThat(actualAssessment.getServiceSourceUiId(), is(CASE_OR_REFERRAL_CMS_BASE10_KEY));
+    assertThat(actualAssessment.getConductedBy(), is("John Smith"));
+  }
+
+  @Test
+  public void putAssessment_validationError_whenUpdatingConductedByOnCompleted()
       throws IOException {
     // given
     final ClientDto person = readObject(FIXTURE_PERSON, ClientDto.class);
@@ -137,18 +336,18 @@ public class AssessmentResourceTest extends AbstractFunctionalTest {
     assessment.setPerson(person);
     assessment.setConductedBy("John Smith");
     assessment.setStatus(AssessmentStatus.COMPLETED);
-    Response response =
+    final AssessmentDto postedAssessment =
         clientTestRule
             .withSecurityToken(AUTHORIZED_NAPA_ACCOUNT_FIXTURE)
             .target(ASSESSMENTS)
             .request(MediaType.APPLICATION_JSON_TYPE)
-            .post(Entity.entity(assessment, MediaType.APPLICATION_JSON_TYPE));
-    assertThat(response.getStatus(), is(HttpStatus.SC_CREATED));
-    final AssessmentDto postedAssessment = response.readEntity(AssessmentDto.class);
-
+            .post(Entity.entity(assessment, MediaType.APPLICATION_JSON_TYPE))
+            .readEntity(AssessmentDto.class);
+    pushToCleanUpStack(postedAssessment.getId(), AUTHORIZED_NAPA_ACCOUNT_FIXTURE);
     // when
+
     postedAssessment.setConductedBy("Other Person");
-    response =
+    Response response =
         clientTestRule
             .withSecurityToken(AUTHORIZED_NAPA_ACCOUNT_FIXTURE)
             .target(ASSESSMENTS + SLASH + postedAssessment.getId())
@@ -157,9 +356,6 @@ public class AssessmentResourceTest extends AbstractFunctionalTest {
 
     // then
     assertThat(response.getStatus(), is(HttpStatus.SC_UNPROCESSABLE_ENTITY));
-
-    // clean up
-    cleanUpAssessments.push(postedAssessment);
   }
 
   private AssessmentDto postAssessment(
