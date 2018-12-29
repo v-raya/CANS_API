3fbaeaf859f1b41d48aa510417666a86e3b37411
Leonid Marushevskyi <leonid.marushevskiy@osi.ca.gov>
2018-12-28 21:22:17 -0800

    CANS-875: prevent altering the county of the client

--- src/main/java/gov/ca/cwds/cans/service/AssessmentService.java
+++ src/main/java/gov/ca/cwds/cans/service/AssessmentService.java
@@ -2,18 +2,25 @@ package gov.ca.cwds.cans.service;
 
 import com.google.inject.Inject;
 import gov.ca.cwds.cans.dao.AssessmentDao;
+import gov.ca.cwds.cans.domain.dto.person.ClientDto;
 import gov.ca.cwds.cans.domain.entity.Assessment;
+import gov.ca.cwds.cans.domain.entity.Person;
 import gov.ca.cwds.cans.domain.enumeration.AssessmentStatus;
+import gov.ca.cwds.cans.domain.mapper.ClientMapper;
 import gov.ca.cwds.cans.domain.search.SearchAssessmentParameters;
+import gov.ca.cwds.rest.exception.ExpectedException;
 import gov.ca.cwds.security.annotations.Authorize;
 import java.util.Collection;
 import java.util.Optional;
+import javax.ws.rs.core.Response.Status;
 
 /** @author denys.davydov */
 public class AssessmentService extends AbstractCrudService<Assessment> {
 
   private final PerryService perryService;
   @Inject private PersonService personService;
+  @Inject private ClientsService clientsService;
+  @Inject private ClientMapper clientMapper;
 
   @Inject
   public AssessmentService(AssessmentDao assessmentDao, PerryService perryService) {
@@ -29,21 +36,26 @@ public class AssessmentService extends AbstractCrudService<Assessment> {
   }
 
   private void createClientIfNeeded(Assessment assessment) {
-    assessment.setPerson(
-        Optional.ofNullable(personService.findByExternalId(assessment.getPerson().getExternalId()))
-            .orElseGet(() -> personService.create(assessment.getPerson())));
+    String clientId = assessment.getPerson().getExternalId();
+    final Person person = getPersonFromLegacy(clientId);
+    Person persistedPerson = Optional.ofNullable(personService.findByExternalId(clientId))
+        .orElseGet(() -> personService.create(person));
+    assessment.setPerson(persistedPerson);
+  }
+
+  private Person getPersonFromLegacy(String clientId) {
+    ClientDto client = clientsService.findByExternalId(clientId);
+    if (client == null) {
+      throw new ExpectedException(
+          "Client [" + clientId + "] is not found in CWS/CMS database.", Status.BAD_REQUEST);
+    }
+    return clientMapper.toPerson(client);
   }
 
   @Override
   public Assessment update(Assessment assessment) {
-    String clientExternalId = assessment.getPerson().getExternalId();
-    assessment.setPerson(
-        Optional.ofNullable(personService.findByExternalId(clientExternalId))
-            .orElseThrow(
-                () ->
-                    new IllegalArgumentException(
-                        "Can't find the client with externalId: " + clientExternalId)));
     assessment.setUpdatedBy(perryService.getOrPersistAndGetCurrentUser());
+    createClientIfNeeded(assessment);
     // TODO: design flow approach
     if (assessment.getStatus() == AssessmentStatus.COMPLETED) {
       Assessment existingAssessment = read(assessment.getId());
